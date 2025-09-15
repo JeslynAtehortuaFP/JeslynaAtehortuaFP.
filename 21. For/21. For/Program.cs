e20ab16579f68e2cbee9fe71d9b0f611c9fb6488
JeslynAtehortuaFP <jeslynatehortuahenao@gmail.com>
2025-09-15 14:29:15 -0500

    CicloFor

---
+++ 21. For/21. For/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _21._For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int i = 1; i <=5; i++)
+            {
+                acumulador=acumulador+i;
+            }
+            Console.WriteLine($"La suma de los primeros 5 numeros es {acumulador}");
+        }
+    }
+}
