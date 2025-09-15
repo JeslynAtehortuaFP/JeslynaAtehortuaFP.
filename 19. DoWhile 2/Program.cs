ce97948780c537ccb69c81b7331b1bb11d2f9469
JeslynAtehortuaFP <jeslynatehortuahenao@gmail.com>
2025-09-15 14:28:16 -0500

    DoWHile

---
+++ 19. DoWhile 2/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace _19._DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int TotalHombres = 0;
+            int TotalMujeres = 0;
+            int MayoresEdad = 0;
+            int MenoresEdad = 0;
+            int Contador = 0;
+
+            do
+            {
+                Console.WriteLine("Alumno #" + (Contador + 1));
+                Console.WriteLine("Ingrese el sexo: H para hombre, M para mujer");
+                string sexo = Console.ReadLine();
+
+                TotalHombres += Convert.ToInt32(sexo == "H" || sexo == "h");
+                TotalMujeres += Convert.ToInt32(sexo == "M" || sexo == "m");
+
+                Console.WriteLine("Ingrese la edad: ");
+
+                string Edad = Console.ReadLine();
+                MayoresEdad += Convert.ToInt32(Convert.ToInt32(Edad) >= 18);
+                MenoresEdad += Convert.ToInt32(Convert.ToInt32(Edad) < 18);
+                Contador++;
+
+            } while (Contador < 10);
+
+            Console.WriteLine("Total de hombres: " + TotalHombres);
+            Console.WriteLine("Total de mujeres: " + TotalMujeres);
+            Console.WriteLine("Total de mayores de edad: " + MayoresEdad);
+            Console.WriteLine("Total de menores de edad: " + MenoresEdad);*/
+
+            int CantidadEstudiantes = 0;
+            int Edad = 0;
+            int genero = 0;
+            int TotalHombres = 0;
+            int TotalMujeres = 0;
+            int MayoresEdad = 0;
+            int MenoresEdad = 0;
+            int cantidad = 0;
+
+            do
+            {
+                Console.WriteLine($"Estudiante {CantidadEstudiantes}");
+                Console.WriteLine("Ingrese la edad: ");
+                Edad = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Ingrese el genero: 1. Hombre 2. Mujer");
+                genero = int.Parse(Console.ReadLine()); 
+
+                if (genero == 1)
+                {
+                    TotalHombres++;
+                }
+                else 
+                {
+                    TotalMujeres++;
+                }
+                if (Edad >= 18)
+                {
+                    MayoresEdad++;
+                }
+                else
+                {
+                    MenoresEdad++;
+                }
+                CantidadEstudiantes++;
+            }while (CantidadEstudiantes < cantidad);
+
+            Console.WriteLine($"Total de hombres: {TotalHombres}");
+            Console.WriteLine($"Total de mujeres: {TotalMujeres}");
+            Console.WriteLine($"Total de mayores de edad: {MayoresEdad}");
+            Console.WriteLine($"Total de menores de edad: {MenoresEdad}");
+
+        }
+    }
+}
---
+++ 20. DoWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _20._DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PartidosJugados = 30;
+            int PartidosGanados = 0;
+            int PartidosPerdidos = 0;
+            int PartidosEmpatados = 0;
+            Double porcentajeGanados = 0;
+            Double porcentajePerdidos = 0;
+            Double porcentajeEmpatados = 0;
+            int contador = 0;
+
+            do
+            {
+                Console.WriteLine("Partido #" + (contador + 1));
+                Console.WriteLine("Ingrese el resultado (G/P/E): G ganado, P perdido, E empatado");
+                string resultado = Console.ReadLine();
+
+                PartidosGanados += Convert.ToInt32(resultado == "G" || resultado == "g");
+                Console.WriteLine("partido ganados: " + PartidosGanados);
+                PartidosPerdidos += Convert.ToInt32(resultado == "P" || resultado == "p");
+                PartidosEmpatados += Convert.ToInt32(resultado == "E" || resultado == "e");
+                contador++;
+                Console.WriteLine();
+
+            } while (contador < PartidosJugados);
+
+            porcentajeGanados = (PartidosGanados *100) / PartidosJugados;
+            porcentajePerdidos = (PartidosPerdidos *100) / PartidosJugados;
+            porcentajeEmpatados = (PartidosEmpatados *100) / PartidosJugados;
+
+            Console.WriteLine("Partidos Ganados: " + PartidosGanados + " - Porcentaje: " + porcentajeGanados + "%");
+            Console.WriteLine("Partidos Perdidos: " + PartidosPerdidos + " - Porcentaje: " + porcentajePerdidos + "%");
+            Console.WriteLine("Partidos Empatados: " + PartidosEmpatados + " - Porcentaje: " + porcentajeEmpatados + "%");
+        }
+    }
+}
